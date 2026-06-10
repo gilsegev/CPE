9ed4c2e3a7e5eaab4a33b2b17430fb3bef589199
Gil Segev <gil.segev@gmail.com>
2026-06-10 17:18:00 -0500

    config: dynamically extract next/image domain whitelist from env variables

--- next.config.js
+++ next.config.js
@@ -1,9 +1,12 @@
 /** @type {import('next').NextConfig} */
+const directusUrl = process.env.NEXT_PUBLIC_DIRECTUS_URL || 'https://directus-production-69c0.up.railway.app';
+const directusHost = new URL(directusUrl).hostname;
+
 const nextConfig = {
   images: {
     domains: [
       "utfs.io",
-      "directus-production-69c0.up.railway.app"
+      directusHost
     ]
   },
   experimental: {
